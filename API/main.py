35c9657a85a085db5eecf0a46792b1685a2b43f9
Anush Bandara <64052389+AnushBandara@users.noreply.github.com>
2025-12-27 07:04:36 +0530

    API setup, Only has to deploy and setup the documentation

--- API/main.py
+++ API/main.py
@@ -7,18 +7,28 @@ from PIL import Image
 import os
 
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
-MODEL_PATH = os.path.join(
+MODEL_PATH_POTATO = os.path.join(
     BASE_DIR,
     "..",
     "Models",
-    "potato_earlyblight_lateblight_healthy_custom_model",
-    "3.keras",
+    "Final",
+    "Potato",
+    "potato_model.keras",
+)
+MODEL_PATH_TOMATO = os.path.join(
+    BASE_DIR,
+    "..",
+    "Models",
+    "Final",
+    "Tomato",
+    "tomato_model.keras",
 )
 
-BLIGHT_MODEL = tf.keras.models.load_model(MODEL_PATH)
-print(BLIGHT_MODEL.summary())
-BLIGHT_CLASS_NAMES = ["Early Blight", "Late Blight", "Healthy"]
+POTATO_MODEL = tf.keras.models.load_model(MODEL_PATH_POTATO)
+TOMATO_MODEL = tf.keras.models.load_model(MODEL_PATH_TOMATO)
 
+POTATO_CLASS_NAMES = ['Bacteria', 'Fungi', 'Healthy', 'Nematode', 'Pest', 'Phytopthora', 'Virus']
+TOMATO_CLASS_NAMES = ['Tomato_Bacterial_spot','Early_blight','Late_blight','Leaf_Mold','Septoria_leaf_spot','Spider_mites_Two_spotted_spider_mite','Target_Spot','YellowLeaf__Curl_Virus', 'mosaic_virus', 'healthy']
 
 def read_file_as_image(data) -> np.ndarray:
     image = Image.open(BytesIO(data)).convert("RGB")
@@ -42,9 +52,25 @@ async def predict_potato_blight(file: UploadFile = File(...)):
         raise HTTPException(status_code=400, detail="Invalid image file")
 
     img_batch = np.expand_dims(image, 0)
-    predictions = BLIGHT_MODEL.predict(img_batch)
+    predictions = POTATO_MODEL.predict(img_batch)
 
-    predicted_class = BLIGHT_CLASS_NAMES[int(np.argmax(predictions[0]))]
+    predicted_class = POTATO_CLASS_NAMES[int(np.argmax(predictions[0]))]
+    confidence = float(np.max(predictions[0]))
+
+    return {"disease": predicted_class, "confidence": confidence}
+
+@app.post("/tomato/")
+async def predict_tomato_disease(file: UploadFile = File(...)):
+    try:
+        contents = await file.read()
+        image = read_file_as_image(contents)
+    except Exception:
+        raise HTTPException(status_code=400, detail="Invalid image file")
+
+    img_batch = np.expand_dims(image, 0)
+    predictions = TOMATO_MODEL.predict(img_batch)
+
+    predicted_class = TOMATO_CLASS_NAMES[int(np.argmax(predictions[0]))]
     confidence = float(np.max(predictions[0]))
 
     return {"disease": predicted_class, "confidence": confidence}
